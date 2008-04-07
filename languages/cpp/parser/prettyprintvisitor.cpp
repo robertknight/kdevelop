d6c14635af493e8899fe6ab633cf21364b8e0444
Robert Knight <robertknight@gmail.com>
2008-04-07 08:14:24 +0100

    Add a visitor which prints an AST out as code.

---
+++ languages/cpp/parser/prettyprintvisitor.cpp
@@ -0,0 +1,616 @@
+
+#include "tokens.h"
+#include "prettyprintvisitor.h"
+
+QString TokenLookup::tokenString(AST* node, std::size_t index) const
+{
+	return token(node,index)->symbol();
+}
+
+TokenStreamTokenLookup::TokenStreamTokenLookup(TokenStream* stream)
+: m_tokens(stream)
+{}
+
+const Token* TokenStreamTokenLookup::token(AST*, std::size_t index) const
+{
+	return &m_tokens->token(index); 
+}
+
+PrettyPrintWriter& PrettyPrintWriter::operator<<(int tokenType)
+{
+	print(tokenType,token_name(tokenType));		
+	return *this;
+}
+PrettyPrintWriter& PrettyPrintWriter::operator<<(const QString& text)
+{
+	print(Token_identifier,text);
+	return *this;
+}
+void SimplePrinter::reset(QTextStream* output)
+{
+	m_output = output;
+	m_lastWasAlphaNum = false;
+	m_lastTokenType = 0;
+	m_indentation = 0;
+}
+void SimplePrinter::enter(AST*)
+{
+}
+void SimplePrinter::leave(AST*)
+{
+}
+void SimplePrinter::print(int tokenType, const QString& text)
+{
+	bool alphaNum = false;
+	for (int i=0;i<text.count();i++)
+		if (text[i].isLetterOrNumber())
+		{
+			alphaNum = true;
+			break;
+		}
+
+	// whitespace based on previous token
+	if (m_lastTokenType == '{' || m_lastTokenType == '}' || m_lastTokenType == ';')
+		if (tokenType != ';')
+			newLine();
+
+	else if (m_lastTokenType == ',')
+	{
+		*m_output << ' ';
+	}
+
+	// whitespace based on current token 
+	if (alphaNum && m_lastWasAlphaNum)
+		*m_output << ' ';
+	else if (tokenType == '{')
+	{
+		newLine();
+		m_indentation++;
+	}
+	else if (tokenType == '}')
+	{
+		m_indentation--;
+		newLine();
+	}
+
+	*m_output << text;
+
+	m_lastWasAlphaNum = alphaNum;
+	m_lastTokenType = tokenType;
+}
+void SimplePrinter::newLine()
+{
+	Q_ASSERT(m_indentation >= 0);
+
+	*m_output << '\n';
+	*m_output << QString(m_indentation,'\t');
+}
+
+PrettyPrintVisitor::PrettyPrintVisitor()
+: m_tokenLookup(0), m_printer(0) , m_currentIndentation(0)
+{}
+void PrettyPrintVisitor::write(QIODevice* device, AST* node)
+{
+	Q_ASSERT(tokenLookup());
+	Q_ASSERT(printer());
+
+	m_outputStream.setDevice(device);
+	m_printer->reset(&m_outputStream);
+	visit(node);
+}
+void PrettyPrintVisitor::visit(AST* node)
+{
+	m_nodeStack.push(node);
+	m_printer->enter(node);
+	DefaultVisitor::visit(node);
+	m_printer->leave(node);
+	m_nodeStack.pop();
+}
+void PrettyPrintVisitor::visitCompoundStatement(CompoundStatementAST* node)
+{
+	*m_printer << '{';
+	DefaultVisitor::visitCompoundStatement(node);
+	*m_printer << '}';
+}
+void PrettyPrintVisitor::visitSimpleTypeSpecifier(SimpleTypeSpecifierAST* node)
+{
+	writeTokenList(node,node->integrals);
+	DefaultVisitor::visitSimpleTypeSpecifier(node);
+}
+void PrettyPrintVisitor::visitUnqualifiedName(UnqualifiedNameAST* node)
+{
+	if (node->tilde != 0)
+		*m_printer << '~';
+	writeToken(node,node->id);
+	DefaultVisitor::visitUnqualifiedName(node);
+}
+void PrettyPrintVisitor::visitExpressionOrDeclarationStatement(ExpressionOrDeclarationStatementAST* node)
+{
+	// ignore ambiguity - treat it as a declaration
+	visit(node->declaration);
+}
+void PrettyPrintVisitor::visitParameterDeclarationClause(ParameterDeclarationClauseAST* node)
+{
+	*m_printer << '(';
+	
+	bool hasDeclarations = node->parameter_declarations != 0;
+	const ListNode<ParameterDeclarationAST*>* declaration = 0;
+	
+	if (hasDeclarations)
+		declaration = node->parameter_declarations->toFront();
+
+	while (declaration != 0)
+	{
+		visitParameterDeclaration(declaration->element);
+
+		if (declaration->hasNext())
+		{
+			declaration = declaration->next;
+			*m_printer << ',';
+		} else
+			break;
+	}
+	
+	if (node->ellipsis != 0)
+	{
+		if (hasDeclarations)
+			*m_printer << ',';
+		*m_printer << Token_ellipsis; 
+	}
+	*m_printer << ')';
+}
+void PrettyPrintVisitor::visitBinaryExpression(BinaryExpressionAST* node)
+{
+	visit(node->left_expression);
+	writeToken(node,node->op);
+	visit(node->right_expression);
+}
+void PrettyPrintVisitor::visitUnaryExpression(UnaryExpressionAST* node)
+{
+	writeToken(node,node->op);
+	visit(node->expression);
+}
+void PrettyPrintVisitor::visitTypeSpecifier(TypeSpecifierAST* node)
+{
+	writeTokenList(node,node->cv);
+}
+void PrettyPrintVisitor::visitAccessSpecifier(AccessSpecifierAST* node)
+{
+	writeTokenList(node,node->specs);
+}
+void PrettyPrintVisitor::visitBaseClause(BaseClauseAST* node)
+{
+	*m_printer << ':';
+	DefaultVisitor::visitBaseClause(node);
+}
+void PrettyPrintVisitor::visitBaseSpecifier(BaseSpecifierAST* node)
+{
+	if (node->virt);
+		*m_printer << Token_virtual;
+	writeToken(node,node->access_specifier);
+	visit(node->name);
+}
+void PrettyPrintVisitor::visitCastExpression(CastExpressionAST* node)
+{
+	*m_printer << '(';
+	visit(node->type_id);
+	*m_printer << ')';
+	*m_printer << '(';
+	visit(node->expression);
+	*m_printer << ')';
+}
+void PrettyPrintVisitor::visitHandler(HandlerAST* node)
+{
+	*m_printer << Token_catch;
+	*m_printer << '(';
+	visit(node->declaration);
+	*m_printer << ')';
+	visit(node->body);
+}
+void PrettyPrintVisitor::visitClassMemberAccess(ClassMemberAccessAST* node)
+{
+	writeToken(node,node->op);
+	visit(node->name);
+}
+void PrettyPrintVisitor::visitClassSpecifier(ClassSpecifierAST* node)
+{
+	visit(node->win_decl_specifiers);
+	writeToken(node,node->class_key);
+	visit(node->name);
+	visit(node->base_clause);
+	*m_printer << '{';
+	visitNodes(this,node->member_specs);
+	*m_printer << '}';
+	*m_printer << ';';
+}
+void PrettyPrintVisitor::visitCppCastExpression(CppCastExpressionAST* node)
+{
+	writeToken(node,node->op);
+	*m_printer << '<';
+	visit(node->type_id);
+	*m_printer << '>';
+	*m_printer << '(';
+	*m_printer << ')';
+
+	visitListWithSeparator(node->sub_expressions,',','(',')');
+}
+void PrettyPrintVisitor::visitCtorInitializer(CtorInitializerAST* node)
+{
+	*m_printer << ':';
+	visitListWithSeparator(node->member_initializers,',');
+}
+void PrettyPrintVisitor::visitDeclarationStatement(DeclarationStatementAST* node)
+{
+	visit(node->declaration);
+}
+void PrettyPrintVisitor::visitDeclarator(DeclaratorAST* node)
+{
+	visitNodes(this,node->ptr_ops);
+	visit(node->sub_declarator);
+	visit(node->id);
+	visit(node->bit_expression);
+	visitNodes(this,node->array_dimensions);
+	visit(node->parameter_declaration_clause);
+	writeTokenList(node,node->fun_cv);
+	visit(node->exception_spec);
+}
+void PrettyPrintVisitor::visitDeleteExpression(DeleteExpressionAST* node)
+{
+	// ISSUE - Make use of node->scope_token
+	*m_printer << Token_delete;
+	
+	if (node->lbracket_token)
+		*m_printer << '[' << ']';
+	
+	visit(node->expression);
+}
+void PrettyPrintVisitor::visitDoStatement(DoStatementAST* node)
+{
+	*m_printer << Token_do;
+	visit(node->statement);
+	*m_printer << Token_while;
+	*m_printer << '(';
+	visit(node->expression);
+	*m_printer << ')';
+	*m_printer << ';';
+}
+void PrettyPrintVisitor::visitElaboratedTypeSpecifier(ElaboratedTypeSpecifierAST* node)
+{
+	writeToken(node,node->type);
+	visit(node->name);
+}
+void PrettyPrintVisitor::visitEnumSpecifier(EnumSpecifierAST* node)
+{
+	*m_printer << Token_enum;
+	visit(node->name);
+	*m_printer << '{';
+	visitListWithSeparator(node->enumerators,',');
+	*m_printer << '}';
+	*m_printer << ';';
+}
+void PrettyPrintVisitor::visitEnumerator(EnumeratorAST* node)
+{
+	writeToken(node,node->id);
+	if (node->expression)
+	{
+		*m_printer << '=';
+		visit(node->expression);
+	}
+}
+void PrettyPrintVisitor::visitExceptionDeclaration(ExceptionDeclarationAST* node)
+{
+	if (node->ellipsis)
+		*m_printer << Token_ellipsis;
+	else
+	{
+		visit(node->type_specifier);
+		visit(node->declarator);
+	}
+}
+void PrettyPrintVisitor::visitExceptionSpecification(ExceptionSpecificationAST* node)
+{
+	*m_printer << Token_throw;
+	*m_printer << '(';
+	if (node->ellipsis)
+		*m_printer << Token_ellipsis;
+	else
+		visitListWithSeparator(node->type_ids,',');
+	*m_printer << ')';
+}
+void PrettyPrintVisitor::visitExpressionStatement(ExpressionStatementAST* node)
+{
+	visit(node->expression);
+	*m_printer << ';';
+}
+void PrettyPrintVisitor::visitFunctionCall(FunctionCallAST* node)
+{
+	*m_printer << '(';
+	visit(node->arguments);
+	*m_printer << ')';
+}
+void PrettyPrintVisitor::visitFunctionDefinition(FunctionDefinitionAST* node)
+{
+	writeTokenList(node,node->storage_specifiers);
+	writeTokenList(node,node->function_specifiers);
+	DefaultVisitor::visitFunctionDefinition(node);
+}
+void PrettyPrintVisitor::visitFunctionTryBlock(FunctionTryBlockAST* )
+{
+	Q_ASSERT(0); // Not implemented yet
+}
+void PrettyPrintVisitor::visitForStatement(ForStatementAST* node)
+{
+	*m_printer << Token_for;
+	*m_printer << '(';
+	visit(node->init_statement);
+	*m_printer << ';';
+	visit(node->condition);
+	*m_printer << ';';
+	visit(node->expression);
+	*m_printer << ')';
+	visit(node->statement);
+	*m_printer << ';';
+}
+void PrettyPrintVisitor::visitIfStatement(IfStatementAST* node)
+{
+	*m_printer << Token_if;
+	*m_printer << '(';
+	visit(node->condition);
+	*m_printer << ')';
+	visit(node->statement);
+
+	if (node->else_statement)
+	{
+		*m_printer << Token_else;
+		visit(node->else_statement);
+	}
+	*m_printer << ';';
+}
+void PrettyPrintVisitor::visitIncrDecrExpression(IncrDecrExpressionAST* node)
+{
+	writeToken(node,node->op);
+}
+void PrettyPrintVisitor::visitInitDeclarator(InitDeclaratorAST* node)
+{
+	DefaultVisitor::visitInitDeclarator(node);
+}
+void PrettyPrintVisitor::visitInitializer(InitializerAST* node)
+{
+	DefaultVisitor::visitInitializer(node);
+}
+void PrettyPrintVisitor::visitInitializerClause(InitializerClauseAST* node)
+{
+	if (node->expression)
+		visit(node->expression);
+	else
+	{
+		*m_printer << '{';
+		visitListWithSeparator(node->initializer_list,',');
+		*m_printer << '}';
+	}
+}
+void PrettyPrintVisitor::visitLabeledStatement(LabeledStatementAST* )
+{
+	Q_ASSERT(0); // Not implemented yet
+}
+void PrettyPrintVisitor::visitLinkageBody(LinkageBodyAST* )
+{
+	Q_ASSERT(0); // Not implemented yet
+}
+void PrettyPrintVisitor::visitLinkageSpecification(LinkageSpecificationAST* )
+{
+	Q_ASSERT(0); // Not implemented yet
+}
+void PrettyPrintVisitor::visitMemInitializer(MemInitializerAST* )
+{
+	Q_ASSERT(0); // Not implemented yet
+}
+void PrettyPrintVisitor::visitName(NameAST* node)
+{
+	visitListWithSeparator(node->qualified_names,Token_scope,0,Token_scope);
+	visit(node->unqualified_name);
+}
+void PrettyPrintVisitor::visitNamespace(NamespaceAST* node)
+{
+	*m_printer << Token_namespace;
+	writeToken(node,node->namespace_name);
+	visit(node->linkage_body);
+}
+void PrettyPrintVisitor::visitNamespaceAliasDefinition(NamespaceAliasDefinitionAST* )
+{
+	Q_ASSERT(0); // Not implemented yet
+}
+void PrettyPrintVisitor::visitNewDeclarator(NewDeclaratorAST* )
+{
+	Q_ASSERT(0); // Not implemented yet
+}
+void PrettyPrintVisitor::visitNewExpression(NewExpressionAST* )
+{
+	Q_ASSERT(0); // Not implemented yet
+}
+void PrettyPrintVisitor::visitNewInitializer(NewInitializerAST* )
+{
+	Q_ASSERT(0); // Not implemented yet
+}
+void PrettyPrintVisitor::visitOperator(OperatorAST* node)
+{
+	*m_printer << Token_operator;
+	writeToken(node,node->op);
+	writeToken(node,node->open);
+	writeToken(node,node->close);
+}
+void PrettyPrintVisitor::visitOperatorFunctionId(OperatorFunctionIdAST* )
+{
+	Q_ASSERT(0); // Not implemented yet
+}
+void PrettyPrintVisitor::visitParameterDeclaration(ParameterDeclarationAST* node)
+{
+	DefaultVisitor::visitParameterDeclaration(node);
+}
+void PrettyPrintVisitor::visitPostfixExpression(PostfixExpressionAST* node)
+{
+	DefaultVisitor::visitPostfixExpression(node);
+}
+void PrettyPrintVisitor::visitPrimaryExpression(PrimaryExpressionAST* node)
+{
+	DefaultVisitor::visitPrimaryExpression(node);	
+}
+void PrettyPrintVisitor::visitPtrOperator(PtrOperatorAST* node)
+{
+	writeToken(node,node->op);
+	writeTokenList(node,node->cv);
+}
+void PrettyPrintVisitor::visitPtrToMember(PtrToMemberAST* )
+{
+	Q_ASSERT(0); // Not implemented yet
+}	
+void PrettyPrintVisitor::visitJumpStatement(JumpStatementAST* node)
+{
+	writeToken(node,node->op);
+	writeToken(node,node->identifier);
+	*m_printer << ';';
+}
+void PrettyPrintVisitor::visitReturnStatement(ReturnStatementAST* node)
+{
+	*m_printer << Token_return;
+	visit(node->expression);
+	*m_printer << ';';
+}
+void PrettyPrintVisitor::visitSimpleDeclaration(SimpleDeclarationAST* node)
+{
+	DefaultVisitor::visitSimpleDeclaration(node);
+	*m_printer << ';';
+}
+void PrettyPrintVisitor::visitSizeofExpression(SizeofExpressionAST* )
+{
+	Q_ASSERT(0); // Not implemented yet
+}
+void PrettyPrintVisitor::visitStringLiteral(StringLiteralAST* node)
+{
+	writeTokenList(node,node->literals);
+}	
+void PrettyPrintVisitor::visitSubscriptExpression(SubscriptExpressionAST* node)
+{
+	*m_printer << '[';
+	visit(node->subscript);
+	*m_printer << ']';
+}
+void PrettyPrintVisitor::visitSwitchStatement(SwitchStatementAST* node)
+{
+	*m_printer << Token_switch;
+	*m_printer << '(';
+	visit(node->condition);
+	*m_printer << ')';
+	visit(node->statement);
+	*m_printer << ';';
+}
+void PrettyPrintVisitor::visitTemplateArgument(TemplateArgumentAST* node)
+{
+	DefaultVisitor::visitTemplateArgument(node);
+}
+void PrettyPrintVisitor::visitTemplateDeclaration(TemplateDeclarationAST* )
+{
+	Q_ASSERT(0); // Not implemented yet
+}
+void PrettyPrintVisitor::visitTemplateParameter(TemplateParameterAST* )
+{
+	Q_ASSERT(0); // Not implemented yet
+}
+void PrettyPrintVisitor::visitThrowExpression(ThrowExpressionAST* node)
+{
+	*m_printer << Token_throw;
+	visit(node->expression);
+}
+void PrettyPrintVisitor::visitTryBlockStatement(TryBlockStatementAST* node)
+{
+	*m_printer << Token_try;
+	visit(node->body);
+	visitNodes(this,node->handlers);
+}
+void PrettyPrintVisitor::visitTypeId(TypeIdAST* node)
+{
+	DefaultVisitor::visitTypeId(node);
+}
+void PrettyPrintVisitor::visitTypeParameter(TypeParameterAST* )
+{
+	Q_ASSERT(0); // Not implemented yet
+}
+void PrettyPrintVisitor::visitTypedef(TypedefAST* node)
+{
+	*m_printer << Token_typedef;
+	visit(node->type_specifier);
+	visitNodes(this,node->init_declarators);
+	*m_printer << ';';
+}
+void PrettyPrintVisitor::visitUsing(UsingAST* node)
+{
+	*m_printer << Token_using;
+	writeToken(node,node->type_name);
+	*m_printer << ';';
+}
+void PrettyPrintVisitor::visitUsingDirective(UsingDirectiveAST* )
+{
+	Q_ASSERT(0); // Not implemented yet
+}
+void PrettyPrintVisitor::visitWhileStatement(WhileStatementAST* node)
+{
+	*m_printer << Token_while;
+	*m_printer << '(';
+	visit(node->condition);
+	*m_printer << ')';
+	visit(node->statement);
+}
+void PrettyPrintVisitor::visitWinDeclSpec(WinDeclSpecAST* )
+{
+	Q_ASSERT(0); // Not implemented yet
+}
+template <class T>
+void PrettyPrintVisitor::visitListWithSeparator(const ListNode<T>* nodes,int separatorToken,
+												int startToken, int endToken)
+{
+	if (!nodes)
+		return;
+
+	if (startToken)
+		*m_printer << startToken;
+
+	const ListNode<T>* node = nodes->toFront();
+	while (node != 0)
+	{
+		visit(node->element);
+		if (node->hasNext())
+		{
+			*m_printer << separatorToken;
+		}
+		else
+			break;
+	}
+
+	if (endToken)
+		*m_printer << endToken;
+}
+void PrettyPrintVisitor::writeToken(AST* node, std::size_t index)
+{
+	const Token* token = m_tokenLookup->token(node,index);
+
+	if (!token)
+		return;
+	else if (token->kind == Token_identifier)
+		*m_printer << token->symbol();
+	else
+		*m_printer << token->kind;
+}
+void PrettyPrintVisitor::writeTokenList(AST* node,const ListNode<std::size_t>* list) 
+{
+	if (list != 0)
+		list = list->toFront();
+
+	while (list != 0)
+	{
+		writeToken(node,list->element);
+		if (list->hasNext())
+			list = list->next;
+		else
+			break;
+	}
+}
---
+++ languages/cpp/parser/prettyprintvisitor.h
@@ -0,0 +1,187 @@
+/* This file is part of KDevelop
+    Copyright 2008 Robert Knight <robertknight@gmail.com>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation
+   or, at your option, any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef PRETTYPRINTVISITOR_H
+#define PRETTYPRINTVISITOR_H
+
+#include <QtCore/QTextStream>
+#include <QtCore/QStack>
+
+#include "default_visitor.h"
+#include "lexer.h"
+#include "ast.h"
+
+class KDEVCPPPARSER_EXPORT TokenLookup
+{
+public:
+	virtual ~TokenLookup() {}
+
+	virtual const Token* token(AST* node, std::size_t index) const = 0; 
+	QString tokenString(AST* node, std::size_t index) const;
+};
+class KDEVCPPPARSER_EXPORT TokenStreamTokenLookup : public TokenLookup
+{
+public:
+	TokenStreamTokenLookup(TokenStream* stream);
+	virtual const Token* token(AST* node, std::size_t index) const;
+private:
+	TokenStream* m_tokens;
+};
+
+class KDEVCPPPARSER_EXPORT PrettyPrintWriter 
+{
+public:
+	virtual ~PrettyPrintWriter() {}
+
+	virtual void reset(QTextStream* output) = 0;
+	virtual void enter(AST* node) = 0;
+	virtual void leave(AST* node) = 0;
+	virtual void print(int tokenType, const QString& text) = 0;
+
+	PrettyPrintWriter& operator<< (int tokenType);
+	PrettyPrintWriter& operator<< (const QString& text);
+};
+class KDEVCPPPARSER_EXPORT SimplePrinter : public PrettyPrintWriter
+{
+public:
+	virtual void reset(QTextStream* output);
+	virtual void enter(AST* node);
+	virtual void leave(AST* node);
+	virtual void print(int tokenType, const QString& text);
+
+private:
+	void newLine();
+
+	QTextStream* m_output;
+	int m_lastTokenType;
+	int m_indentation;
+	bool m_lastWasAlphaNum:1;
+};
+
+class KDEVCPPPARSER_EXPORT PrettyPrintVisitor : protected DefaultVisitor
+{
+public:
+	PrettyPrintVisitor();
+
+	void setTokenLookup(TokenLookup* lookup);
+	TokenLookup* tokenLookup() const;
+
+	void setPrinter(PrettyPrintWriter* rules);	
+	PrettyPrintWriter* printer() const;
+
+	void write(QIODevice* device, AST* node);
+
+protected:
+	virtual void visit(AST* node);
+	virtual void visitAccessSpecifier(AccessSpecifierAST *);
+	virtual void visitBaseClause(BaseClauseAST *);
+	virtual void visitBaseSpecifier(BaseSpecifierAST *);
+	virtual void visitBinaryExpression(BinaryExpressionAST *);
+	virtual void visitCastExpression(CastExpressionAST *);
+	virtual void visitClassMemberAccess(ClassMemberAccessAST *);
+	virtual void visitClassSpecifier(ClassSpecifierAST *);
+	virtual void visitCompoundStatement(CompoundStatementAST *);
+	virtual void visitCppCastExpression(CppCastExpressionAST *);
+	virtual void visitCtorInitializer(CtorInitializerAST *);
+	virtual void visitDeclarationStatement(DeclarationStatementAST *);
+	virtual void visitDeclarator(DeclaratorAST *);
+	virtual void visitDeleteExpression(DeleteExpressionAST *);
+	virtual void visitDoStatement(DoStatementAST *);
+	virtual void visitElaboratedTypeSpecifier(ElaboratedTypeSpecifierAST *);
+	virtual void visitEnumSpecifier(EnumSpecifierAST *);
+	virtual void visitEnumerator(EnumeratorAST *);
+	virtual void visitExceptionDeclaration(ExceptionDeclarationAST *);
+	virtual void visitExceptionSpecification(ExceptionSpecificationAST *);
+	virtual void visitExpressionOrDeclarationStatement(ExpressionOrDeclarationStatementAST *);
+	virtual void visitExpressionStatement(ExpressionStatementAST *);
+	virtual void visitForStatement(ForStatementAST *);
+	virtual void visitFunctionCall(FunctionCallAST *);
+	virtual void visitHandler(HandlerAST *);
+	virtual void visitFunctionDefinition(FunctionDefinitionAST *);
+	virtual void visitFunctionTryBlock(FunctionTryBlockAST *);
+	virtual void visitIfStatement(IfStatementAST *);
+	virtual void visitIncrDecrExpression(IncrDecrExpressionAST *);
+	virtual void visitInitDeclarator(InitDeclaratorAST *);
+	virtual void visitInitializer(InitializerAST *);
+	virtual void visitInitializerClause(InitializerClauseAST *);
+	virtual void visitJumpStatement(JumpStatementAST *);
+	virtual void visitLabeledStatement(LabeledStatementAST *);
+	virtual void visitLinkageBody(LinkageBodyAST *);
+	virtual void visitLinkageSpecification(LinkageSpecificationAST *);
+	virtual void visitMemInitializer(MemInitializerAST *);
+	virtual void visitName(NameAST *);
+	virtual void visitNamespace(NamespaceAST *);
+	virtual void visitNamespaceAliasDefinition(NamespaceAliasDefinitionAST *);
+	virtual void visitNewDeclarator(NewDeclaratorAST *);
+	virtual void visitNewExpression(NewExpressionAST *);
+	virtual void visitNewInitializer(NewInitializerAST *);
+	virtual void visitOperator(OperatorAST *);
+	virtual void visitOperatorFunctionId(OperatorFunctionIdAST *);
+	virtual void visitParameterDeclaration(ParameterDeclarationAST *);
+	virtual void visitParameterDeclarationClause(ParameterDeclarationClauseAST *);
+	virtual void visitPostfixExpression(PostfixExpressionAST *);
+	virtual void visitPrimaryExpression(PrimaryExpressionAST *);
+	virtual void visitPtrOperator(PtrOperatorAST *);
+	virtual void visitPtrToMember(PtrToMemberAST *);
+	virtual void visitReturnStatement(ReturnStatementAST *);
+	virtual void visitSimpleDeclaration(SimpleDeclarationAST *);
+	virtual void visitSimpleTypeSpecifier(SimpleTypeSpecifierAST *);
+	virtual void visitSizeofExpression(SizeofExpressionAST *);
+	virtual void visitStringLiteral(StringLiteralAST *);
+	virtual void visitSubscriptExpression(SubscriptExpressionAST *);
+	virtual void visitSwitchStatement(SwitchStatementAST *);
+	virtual void visitTemplateArgument(TemplateArgumentAST *);
+	virtual void visitTemplateDeclaration(TemplateDeclarationAST *);
+	virtual void visitTemplateParameter(TemplateParameterAST *);
+	virtual void visitThrowExpression(ThrowExpressionAST *);
+	virtual void visitTryBlockStatement(TryBlockStatementAST *);
+	virtual void visitTypeId(TypeIdAST *);
+	virtual void visitTypeParameter(TypeParameterAST *);
+	virtual void visitTypeSpecifier(TypeSpecifierAST *);
+	virtual void visitTypedef(TypedefAST *);
+	virtual void visitUnaryExpression(UnaryExpressionAST *);
+	virtual void visitUnqualifiedName(UnqualifiedNameAST *);
+	virtual void visitUsing(UsingAST *);
+	virtual void visitUsingDirective(UsingDirectiveAST *);
+	virtual void visitWhileStatement(WhileStatementAST *);
+	virtual void visitWinDeclSpec(WinDeclSpecAST *);
+	
+private:
+	void writeToken(AST* node, std::size_t token);
+	void writeTokenList(AST* node,const ListNode<std::size_t>* tokens);
+	template <class T>
+	void visitListWithSeparator(const ListNode<T>* nodes,int separatorToken,
+								int startToken = 0, int endToken = 0);
+	
+	QStack<AST*> m_nodeStack;
+	TokenLookup* m_tokenLookup;
+	PrettyPrintWriter* m_printer;
+	int m_currentIndentation;
+	QTextStream m_outputStream;
+};
+inline void PrettyPrintVisitor::setTokenLookup(TokenLookup* lookup)
+{ m_tokenLookup = lookup; }
+inline void PrettyPrintVisitor::setPrinter(PrettyPrintWriter* rules)
+{ m_printer = rules; }
+inline TokenLookup* PrettyPrintVisitor::tokenLookup() const
+{ return m_tokenLookup; }
+inline PrettyPrintWriter* PrettyPrintVisitor::printer() const
+{ return m_printer; }
+
+#endif // PRETTYPRINTVISITOR_H 
